38523186bd7d4ee04f405a1630cdd03fae8a7079
不忘初心 <1624717079@qq.com>
2022-04-02 18:14:34 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ set -x
 sed -i -e "s|###appId###|$appId|g" weAppCode.php && \
 sed -i -e "s|###weapp_secret###|$weapp_secret|g" weAppCode.php && \
 sed -i -e "s|###appId2###|$appId|g" weAppCodeyj.php && \
-sed -i -e "s|###weapp_secret2###|$weapp_secret|g" weAppCode.phpyj && \
+sed -i -e "s|###weapp_secret2###|$weapp_secret|g" weAppCodeyj.php && \
 sed -i -e "s|###imgpath###|$imgpath|g" weAppCode.php
 sed -i -e "s|###imgpath###|$imgpath|g" weAppCodeyj.php
 
